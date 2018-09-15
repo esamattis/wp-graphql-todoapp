6fc651ea3debaa0b339c8b3bf52c361d3881bf80
Esa-Matti Suuronen <esa-matti@suuronen.org>
2018-09-15 20:34:43 +0300

    sync stuff

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const {task, parallel} = require("gulp");
+const {sh} = require("gulp-sh");
+
+const backendFilesGlob = ["*.php", "*.html", "lib"];
+
+function wrapQuotes(globs) {
+    return globs.map(s => `"${s}"`);
+}
+task(
+    "sync-backend-dev",
+    sh`
+    chokidar ${wrapQuotes(
+        backendFilesGlob,
+    )} -c 'rsync -Pah  ${backendFilesGlob} git@graphql.valudata-fi.test:sites/graphql/wp-content/themes/wp-graphql-todoapp/'
+`,
+);
