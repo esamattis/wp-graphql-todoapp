e147d3ff50d8261f10343113264b78930edf6f27
Esa-Matti Suuronen <esa-matti@suuronen.org>
2018-09-16 12:22:06 +0300

    Cleanup babel config

--- .babelrc.js
+++ .babelrc.js
@@ -1,35 +1,41 @@
 // XXX: There must be a better way
 const IS_JEST = process.argv.join("").includes("jest");
 
-// only for es imports
-const nodeSupport = [
-    "@babel/preset-env",
-    {
-        targets: {
-            node: "current",
+function addNodeSupport(config) {
+    // only for es imports
+    config.presets.push([
+        "@babel/preset-env",
+        {
+            targets: {
+                node: "current",
+            },
         },
-    },
-];
+    ]);
+}
 
-const browserSupport = [
-    "@babel/preset-env",
-    {
-        // http://browserl.ist/?q=%3E+0.5%25%2C+not+dead%2C+not+samsung+%3C%3D+4%2C+not+android+%3E+0%2C+not+op_mini+%3E+0%2C+not+ie+%3E+0
-        targets: [
-            "> 0.5%",
-            "not dead",
-            "not samsung <= 4",
-            "not android > 0",
-            "not op_mini > 0",
-            "not ie > 0 ",
-        ],
-        // We will transform async functions to generators.
-        // It gives  us smaller bundlers and better debugging experience
-        exclude: ["transform-regenerator"],
-    },
-];
+function addModernBrowserSupport(config) {
+    config.presets.push([
+        "@babel/preset-env",
+        {
+            // http://browserl.ist/?q=%3E+0.5%25%2C+not+dead%2C+not+samsung+%3C%3D+4%2C+not+android+%3E+0%2C+not+op_mini+%3E+0%2C+not+ie+%3E+0
+            targets: [
+                "> 0.5%",
+                "not dead",
+                "not samsung <= 4",
+                "not android > 0",
+                "not op_mini > 0",
+                "not ie > 0 ",
+            ],
+            // We will transform async functions to generators.
+            // It gives  us smaller bundlers and better debugging experience
+            exclude: ["transform-regenerator"],
+        },
+    ]);
+    // The generator transform for async functions
+    config.plugins.push("@babel/plugin-transform-async-to-generator");
+}
 
-let config = {
+let CONFIG = {
     presets: ["@babel/preset-typescript", "@babel/preset-react"],
     plugins: [
         "@babel/plugin-proposal-class-properties",
@@ -38,11 +44,9 @@ let config = {
 };
 
 if (IS_JEST) {
-    config.presets.push(nodeSupport);
+    addNodeSupport(CONFIG);
 } else {
-    config.presets.push(browserSupport);
-    // The generator transform for async functions
-    config.plugins.push("@babel/plugin-transform-async-to-generator");
+    addModernBrowserSupport(CONFIG);
 }
 
-module.exports = config;
+module.exports = CONFIG;
