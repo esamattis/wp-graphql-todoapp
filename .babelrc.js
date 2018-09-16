5bc01574738dc229d3ab281186313c0aba1e4abc
Esa-Matti Suuronen <esa-matti@suuronen.org>
2018-09-16 12:12:04 +0300

    use preset env

--- .babelrc.js
+++ .babelrc.js
@@ -11,6 +11,24 @@ const nodeSupport = [
     },
 ];
 
+const browserSupport = [
+    "@babel/preset-env",
+    {
+        // http://browserl.ist/?q=%3E+0.5%25%2C+not+dead%2C+not+samsung+%3C%3D+4%2C+not+android+%3E+0%2C+not+op_mini+%3E+0%2C+not+ie+%3E+0
+        targets: [
+            "> 0.5%",
+            "not dead",
+            "not samsung <= 4",
+            "not android > 0",
+            "not op_mini > 0",
+            "not ie > 0 ",
+        ],
+        // We will transform async functions to generators.
+        // It gives  us smaller bundlers and better debugging experience
+        exclude: ["transform-regenerator"],
+    },
+];
+
 let config = {
     presets: ["@babel/preset-typescript", "@babel/preset-react"],
     plugins: [
@@ -21,6 +39,10 @@ let config = {
 
 if (IS_JEST) {
     config.presets.push(nodeSupport);
+} else {
+    config.presets.push(browserSupport);
+    // The generator transform for async functions
+    config.plugins.push("@babel/plugin-transform-async-to-generator");
 }
 
 module.exports = config;
