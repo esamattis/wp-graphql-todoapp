2ec88098283ed0aadd47779519454ab6e80275fd
Esa-Matti Suuronen <esa-matti@suuronen.org>
2018-09-16 21:42:21 +0300

    Body meta tags

--- index.php
+++ index.php
@@ -11,6 +11,14 @@ http_response_code(200);
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css?family=Signika" rel="stylesheet">
+    <title>WP GraphQL Todo App</title>
+    <style>
+    html, body {
+        padding: 0;
+        margin: 0;
+    }
+    </style>
 </head>
 
 <body>
