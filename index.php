1ba43f31cc4e3db005362d6dddda3fd35896da9e
Esa-Matti Suuronen <esa-matti@suuronen.org>
2018-09-16 13:29:38 +0300

    fixup! Basic index.php

--- index.php
+++ index.php
@@ -1,22 +1,24 @@
 <?php
 
-http_response_code(200); ?>
-<!doctype html>
-<html>
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-</head>
-
-<body>
-    <noscript>
-        You need to enable JavaScript to run this app.
-    </noscript>
-    <div id="root"></div>
-    <script>
-        <?php readfile(__DIR__ . '/src/bootstrap.js'); ?>
-    </script>
-</body>
-
+http_response_code(200  );
+
+?>
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+</head>
+
+<body>
+    <noscript>
+        You need to enable JavaScript to run this app.
+    </noscript>
+    <div id="root"></div>
+    <script>
+        <?php readfile(__DIR__ . '/src/bootstrap.js'); ?>
+    </script>
+</body>
+
 </html>
